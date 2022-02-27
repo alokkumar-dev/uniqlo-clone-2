4700a93862df5f8c286bc9ecae0cd30d91dba84b
alokkumar-dev <alokshakya2601@gmail.com>
2022-02-27 16:06:53 +0530

    fin

--- scripts/mens_ultra-light-down.js
+++ scripts/mens_ultra-light-down.js
@@ -24,194 +24,172 @@
 //         desc: "Exclusive Size Online Only",
 //         price: 2990
 //     },
-   
-   
 
 // ];
 // console.log(newArrivalObj_1[0].price+newArrivalObj_1[1].price);
 // displayProducts_1(newArrivalObj_1);
 
-
+let data;
 async function searchVideo() {
-    try {
+  try {
+    let responce = await fetch(`https://uniqlo-clone.herokuapp.com/products`);
 
-        let responce = await fetch(`https://uniqlo-clone.herokuapp.com/products`);
+    data = await responce.json();
+    data = data.products;
+    console.log("Data : ", data.products);
 
-        let data = await responce.json();
-        console.log("Data : ", data.products);
-
-        displayProducts_1(data.products);
-
-    }
-    catch (err) {
-        console.log("Error", err);
-    }
-
-
+    displayProducts_1(data.products);
+  } catch (err) {
+    console.log("Error", err);
+  }
 }
 searchVideo();
 
 function displayProducts_1(data) {
-    document.querySelector(".productSide").innerHTML="";
-    data.map(({ image, category, size, name,description, price }) => {
-        let mainDiv = document.createElement("div");
-        let imgDiv = document.createElement("div");
-        let sizeDiv = document.createElement("div");
+  document.querySelector(".productSide").innerHTML = "";
+  data.map(({ image, category, size, name, description, price }) => {
+    let mainDiv = document.createElement("div");
+    let imgDiv = document.createElement("div");
+    let sizeDiv = document.createElement("div");
 
+    let img = document.createElement("img");
+    let gend = document.createElement("h3");
+    let siz = document.createElement("h3");
+    let des = document.createElement("h2");
+    let exclus = document.createElement("h4");
+    let pric = document.createElement("h4");
 
-        let img = document.createElement("img");
-        let gend = document.createElement("h3");
-        let siz = document.createElement("h3");
-        let des = document.createElement("h2");
-        let exclus = document.createElement("h4");
-        let pric = document.createElement("h4");
+    imgDiv.setAttribute("class", "imgDiv");
+    sizeDiv.setAttribute("class", "sizeDiv");
+    exclus.setAttribute("class", "exclusive");
 
-        imgDiv.setAttribute("class", "imgDiv");
-        sizeDiv.setAttribute("class", "sizeDiv");
-        exclus.setAttribute("class", "exclusive");
-
-        let productsObj = {
-            image,
-            category,
-            size,
-            name,
-            description,
-            price
-        }
-        mainDiv.addEventListener("click", () => {
-            storeData(productsObj);
-        });
-
-        img.src = image;
-        gend.innerText = category;
-        siz.innerText = size;
-        des.innerText = name;
-        exclus.innerText = description;
-        pric.innerText = `Rs. ${price}.00`;
-
-        imgDiv.append(img);
-        sizeDiv.append(gend, siz);
-        mainDiv.append(imgDiv, sizeDiv, des, exclus, pric);
-
-        document.querySelector(".productSide").append(mainDiv);
+    let productsObj = {
+      image,
+      category,
+      size,
+      name,
+      description,
+      price,
+    };
+    mainDiv.addEventListener("click", () => {
+      storeData(productsObj);
     });
 
+    img.src = image;
+    gend.innerText = category;
+    siz.innerText = size;
+    des.innerText = name;
+    exclus.innerText = description;
+    pric.innerText = `Rs. ${price}.00`;
+
+    imgDiv.append(img);
+    sizeDiv.append(gend, siz);
+    mainDiv.append(imgDiv, sizeDiv, des, exclus, pric);
+
+    document.querySelector(".productSide").append(mainDiv);
+  });
 }
 
 var sort = document.getElementById("sortProduct");
 sort.addEventListener("change", function priceSort() {
-    var selected = document.getElementById("sortProduct").value;
-    if (selected == "low") {
-        newArrivalObj_1.sort(function (a, b) {
-            // console.log(a.price)
-            return a.price - b.price;
+  var selected = document.getElementById("sortProduct").value;
+  if (selected == "low") {
+    data.sort(function (a, b) {
+      // console.log(a.price)
+      return a.price - b.price;
+    });
+  }
+  if (selected == "high") {
+    data.sort(function (a, b) {
+      // console.log(b.price)
+      return b.price - a.price;
+    });
+  }
 
-        })
-    }
-    if (selected == "high") {
-        newArrivalObj_1.sort(function (a, b) {
-            // console.log(b.price)
-            return b.price - a.price;
-        })
-    }
-    
-    displayProducts_1(newArrivalObj_1);
+  displayProducts_1(data);
 });
 
-
 function storeData(data) {
-    localStorage.setItem("uniqProd", JSON.stringify(data));
-    window.location.href = "./productinfo.html";
+  localStorage.setItem("uniqProd", JSON.stringify(data));
+  window.location.href = "./productinfo.html";
 }
 
-//for selected option 
+//for selected option
 
 var selected1 = document.querySelector(".select1");
 selected1.addEventListener("change", () => {
-
-    let select = document.querySelector(".select1").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select1").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected2 = document.querySelector(".select2");
 selected2.addEventListener("change", () => {
-
-    let select = document.querySelector(".select2").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select2").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected3 = document.querySelector(".select3");
 selected3.addEventListener("change", () => {
-
-    let select = document.querySelector(".select3").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select3").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected4 = document.querySelector(".select4");
 selected4.addEventListener("change", () => {
-
-    let select = document.querySelector(".select4").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select4").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected5 = document.querySelector(".select5");
 selected5.addEventListener("change", () => {
-
-    let select = document.querySelector(".select5").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select5").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected6 = document.querySelector(".select6");
 selected6.addEventListener("change", () => {
-
-    let select = document.querySelector(".select6").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select6").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected7 = document.querySelector(".select7");
 selected7.addEventListener("change", () => {
-
-    let select = document.querySelector(".select7").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select7").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected8 = document.querySelector(".select8");
 selected8.addEventListener("change", () => {
-
-    let select = document.querySelector(".select8").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select8").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected9 = document.querySelector(".select9");
 selected9.addEventListener("change", () => {
-
-    let select = document.querySelector(".select9").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select9").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected10 = document.querySelector(".select10");
 selected10.addEventListener("change", () => {
-
-    let select = document.querySelector(".select10").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select10").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
 var selected11 = document.querySelector(".select11");
 selected11.addEventListener("change", () => {
-
-    let select = document.querySelector(".select11").value;
-    if (select != "") {
-        window.location.href = `./${select}.html`;
-    }
+  let select = document.querySelector(".select11").value;
+  if (select != "") {
+    window.location.href = `./${select}.html`;
+  }
 });
-
